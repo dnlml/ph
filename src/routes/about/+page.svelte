f470885a5acb8558714f8f7a3f15bdb7cdd0bb72
Daniele <danielemeli86@gmail.com>
2023-10-06 16:42:59 +0200

    LAst images

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,4 +1,20 @@
-<section class="grid md:grid-cols-2">
-  <div>About me</div>
-  <div>foto</div>
+<script lang="ts">
+  import Img from '@zerodevx/svelte-img';
+  import Me from '$lib/assets/images/me.jpg?as=run';
+</script>
+
+<section class="grid md:grid-cols-2 min-h-[100dvh] gap-12 items-center md:-mt-28">
+  <div class="max-w-xl text-center justify-self-center md:justify-self-end">
+    <p>
+      Italian photographer Daniele based in Barcelona since 2014.
+      <br />
+      He considers the world a playground to explore within different disciplines.
+      <br />
+      Photography being one of particular interest, he discovered the slow analogue process, the creativity
+      behind the creation of a photobook and the beauty of this art.
+    </p>
+  </div>
+  <div class="max-w-xl justify-self-center md:justify-self-start">
+    <Img src={Me} />
+  </div>
 </section>
