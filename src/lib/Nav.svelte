1191fee44db95b149beb88f623808ec285de9496
Daniele <danielemeli86@gmail.com>
2023-10-08 15:36:26 +0200

    remove mix blend mode

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,6 +1,4 @@
-<nav
-  class="nav h-16 md:h-28 fixed top-0 right-0 left-0 z-30 mix-blend-difference px-3 md:px-6 pointer-events-none"
->
+<nav class="nav h-16 md:h-28 fixed top-0 right-0 left-0 z-30 px-3 md:px-6 pointer-events-none">
   <div class="flex justify-between items-center h-full">
     <h1 class="text-xl">
       <a class="pointer-events-auto" href="/">Daniele Meli</a>
