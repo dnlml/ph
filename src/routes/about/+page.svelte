82a7c3dc4b26e31bb1aa0eaae95c495d302d8617
Daniele <danielemeli86@gmail.com>
2024-07-08 14:55:31 +0200

    maazing thing nobody understand

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,6 +1,17 @@
 <script lang="ts">
   import Img from '@zerodevx/svelte-img';
+  // import Dm from '$lib/assets/images/dm.jpg?as=run';
   import Me from '$lib/assets/images/me.jpg?as=run';
+
+  const a = [1, 2, 3, 4, 5];
+
+  for (let i = 0, j = a.length; i < j; i++) {
+    if (a[i] === 5) {
+      a[i] = 9;
+    }
+  }
+
+  console.log(a);
 </script>
 
 <section class="grid md:grid-cols-2 min-h-[100dvh] gap-12 items-center md:-mt-28">
@@ -15,6 +26,7 @@
     </p>
   </div>
   <div class="max-w-xl justify-self-center md:justify-self-start">
+    <!-- <Img src={Dm} /> -->
     <Img src={Me} />
   </div>
 </section>
